2e277e7f4dc9ef5678ecb97266dcaa835a1df22e
arslanah99 <arah09@hotmail.com>
2020-08-02 00:59:17 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [cardData, setCardData] = useState([]);
+  const [visible, setVisible] = useState(5);
+
+  const allCardData = async () => {
+    const response = await axios.get("https://randomuser.me/api/?results=35");
+    setCardData(response.data.results);
+  };
+
+  const loadMore = () => {
+    setVisible(visible + 5);
+  };
+
+  useEffect(() => {
+    allCardData();
+  }, []);
+
+  const renderCard = (person, index) => {
+    return (
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={person.picture.large} />
+        <Card.Body>
+          <Card.Title>
+            {person.name.first} {person.name.last}
+          </Card.Title>
+          <Card.Text>
+            <ul>
+              <li>{person.email}</li>
+              <li>{person.cell}</li>
+              <li>{person.gender}</li>
+            </ul>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <div className="cards">
+          {cardData.slice(0, visible).map(renderCard)}
+        </div>
+      </div>
+      {visible < cardData.length && (
+        <button onClick={loadMore}>Load 5 More</button>
+      )}
     </div>
   );
-}
+};
 
 export default App;
